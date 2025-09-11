3bbb61117b26eefdc79fc90cc9e6fc0542ac47c9
Larissa Vicenzi <vicenzi.pessoal@gmail.com>
2025-09-11 10:04:02 -0300

    Adicionando fromRaw na classe post para resgatar dados do id

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,17 @@
 let button = document.getElementById('btnUsers');
 
 button.addEventListener('click', function(){
-    fetch('https://jsonplaceholder.typicode.com/posts/1')
+    fetch('https://jsonplaceholder.typicode.com/posts77')
     .then(function (response){
         return response.json();
     })
     .then(function (json) {
-        div.innerText = json.body;
+        // let post = new Post (1,1,"","");
+        let post = Post.fromRaw(json);
+
+        console.log(post);
+        
+        // div.innerText = json.body;
         // div.innerText = JSON.stringify(json, null, 0);
         
     })
